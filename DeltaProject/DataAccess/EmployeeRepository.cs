47f7516584a35ab4c77a1200c4f7d8096e7c7815
Kristian Leonhard <kristian.leonhard@anticimex.com>
2024-07-07 12:35:28 +0200

    Binding Employees to Assigned To filter in the TasksWindow.

--- DeltaProject/DataAccess/EmployeeRepository.cs
+++ DeltaProject/DataAccess/EmployeeRepository.cs
@@ -6,8 +6,42 @@ using System.Data;
 
 namespace DeltaProject.DataAccess
 {
-    public class EmployeeRepository : Repository
+    public class EmployeeRepository : Repository, IEnumerable<Employee>
     {
+        private List<Employee> list = new List<Employee>();
+
+        public IEnumerator<Employee> GetEnumerator()
+        {
+            return list.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void GetAll()
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand("SELECT EmployeeId, Name, Address, WorkEmail, SocialSecurityNumber, Phone, WorkPhone, Password FROM Employee", connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                list.Clear();
+                while (reader.Read())
+                    list.Add(new Employee((int)reader[0], reader[1].ToString(), reader[2].ToString(), reader[3].ToString(), reader[4].ToString(), reader[5].ToString(), reader[6].ToString(), reader[7].ToString()));
+                OnChanged(DbOperation.SELECT, DbModeltype.Department);
+            }
+            catch (Exception ex)
+            {
+                throw new DbException("Error in Employee repository: " + ex.Message);
+            }
+            finally
+            {
+                if (connection != null && connection.State == ConnectionState.Open) connection.Close();
+            }
+        }
+
         public Employee ValidateLogin(string workEmail, string password)
         {
             try
--- DeltaProject/ViewModel/TasksViewModel.cs
+++ DeltaProject/ViewModel/TasksViewModel.cs
@@ -26,10 +26,23 @@ namespace DeltaProject.ViewModel
             }
         }
 
+        private string _selectedAssignedTo;
+        public string SelectedAssignedTo
+        {
+            get { return _selectedAssignedTo; }
+            set
+            {
+                _selectedAssignedTo = value;
+                OnPropertyChanged(nameof(SelectedAssignedTo));
+                FilterTasks();
+            }
+        }
+
         public ICommand AddTaskCommand { get; set; }
         public ICommand ResetDepartmentsFilterCommand { get; set; }
 
         public static DepartmentRepository departmentRepository = new DepartmentRepository();
+        public static EmployeeRepository employeeRepository = new EmployeeRepository();
 
         public TasksViewModel()
         {
@@ -41,7 +54,8 @@ namespace DeltaProject.ViewModel
             departmentRepository.RepositoryChanged += RefreshDepartments;
             departmentRepository.GetAll();
 
-            AssignedToList = new ObservableCollection<string> { "Dr. Hansen", "Dr. Nielsen", "Dr. Pedersen", "Dr. Rasmussen" };
+            employeeRepository.RepositoryChanged += RefreshAssignedToList;
+            employeeRepository.GetAll();
 
             _selectedDepartments = new ObservableCollection<string>();
             _selectedDepartments.CollectionChanged += (s, e) => FilterTasks();
@@ -52,12 +66,19 @@ namespace DeltaProject.ViewModel
 
         private void RefreshDepartments(object sender, DbEventArgs e)
         {
-            Departments = new ObservableCollection<string>(departmentRepository.Select(d => d.Name));            
+            Departments = new ObservableCollection<string>(departmentRepository.Select(d => d.Name));
+            OnPropertyChanged(nameof(Departments));
+        }
+
+        private void RefreshAssignedToList(object sender, DbEventArgs e)
+        {
+            AssignedToList = new ObservableCollection<string>(employeeRepository.Select(em => em.Name));
+            OnPropertyChanged(nameof(AssignedToList));
         }
 
         private void FilterTasks()
         {
-            // Implement task filtering logic based on selected departments
+            // Implement task filtering logic based on selected departments and assigned to
         }
 
         private void AddTask(object parameter)
