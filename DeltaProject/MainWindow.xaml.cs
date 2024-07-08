994135e63fcc10b17908a514eb304f8d57b728cc
Kristian Leonhard <kristian.leonhard@anticimex.com>
2024-07-08 22:13:52 +0200

    Changed the checkboxes on the Login page so no need for double click.

--- DeltaProject/MainWindow.xaml.cs
+++ DeltaProject/MainWindow.xaml.cs
@@ -1,22 +1,45 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 using DeltaProject.ViewModel;
 using DeltaProject.Model;
 
 namespace DeltaProject
 {
-  public partial class MainWindow : Window
-  {
-    private MainViewModel model = new MainViewModel();
-
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-      InitializeComponent();
-      DataContext = model;
-      model.WarningHandler += delegate(object sender, MessageEventArgs e) { 
-          MessageBox.Show(e.Message, "Warning", MessageBoxButton.OK, MessageBoxImage.Warning); 
-      };
+        private MainViewModel model = new MainViewModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = model;
+            model.WarningHandler += delegate (object sender, MessageEventArgs e) {
+                MessageBox.Show(e.Message, "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+            };
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var viewModel = DataContext as MainViewModel;
+            if (viewModel != null)
+            {
+                foreach (var item in e.AddedItems)
+                {
+                    var location = item as Location;
+                    if (location != null)
+                    {
+                        location.IsSelected = true;
+                    }
+                }
+                foreach (var item in e.RemovedItems)
+                {
+                    var location = item as Location;
+                    if (location != null)
+                    {
+                        location.IsSelected = false;
+                    }
+                }
+            }
+        }
     }
-  }
 }
